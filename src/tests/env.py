322915ed6128cac37d8c243b1e8f1ab49cb1a491
AishwaryaMaddula <156866302+AishwaryaMaddula@users.noreply.github.com>
2025-09-21 00:58:29 -0400

    env.py v1.0

---
+++ src/tests/env.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+
+class PseudoLabelEnv(gym.Env):
+    def __init__(self):
+        super().__init__()
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass
